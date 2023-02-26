1e781d4d76e2fe81a204e02d32aaced3cabd531a
rpgjr <rodrigo21geneta@gmail.com>
2023-02-27 06:17:03 +0800

    Polished System V4.2

--- resources/views/mail/reset-password.blade.php
+++ resources/views/mail/reset-password.blade.php
@@ -80,18 +80,25 @@ Click the button below to redirect you to the password reset page.
         <center>
             <div class="logo">
                 <img src="{{ asset('img/pupLogo.png') }}" alt="" class="header-logo">
-                <img src="{{ asset('img/pupapsLogo.png') }}" alt="" class="header-logo">
+                <img src="{{ asset('img/pupt-aps-logo-2.png') }}" alt="" class="header-logo">
             </div>
-            <p class="header">PUPT - Alumni Portal System</p>
+            <p class="header">Polytechnic University of the Philippines Taguig Branch
+                <span style="font-size: 25px; font-weight: bold;"><br>Alumni Portal System</span>
+            </p>
         </center>
 
     <br>
-    <p>Hi {{ $username }},</p>
+    <p>Greetings {{ $username }},</p>
     <br>
-    <p>We received a request to reset the password for your account. If you did not initiate this request, please disregard this email.</p>
+    <p>We received a request to reset the password for your account. To reset your password, kindly click on the button below to proceed to the reset password page. After reseting your password, please login to your account. If you did not initiate this request, please disregard this email.</p>
 
+    <br>
+    <p>Padayon!</p>
+
+    <br>
+    <br>
     @component('mail::button', ['url' => route('login.changePassword', $email)])
-        GO TO PASSWORD RESET
+        GO TO PASSWORD RESET PAGE
     @endcomponent
 
     <p style="font-size: 10px; text-align: center;">&#169;PUPT-Alumni Portal System
